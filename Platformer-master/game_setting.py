5eb418cf4c684b8b9b73962a6a253f3167ce0ccb
jjrm <yeji_min@naver.com>
2022-12-09 02:14:26 +0900

    [22.12.09] UPDATE 게임 롤, 게임세팅 모듈화

---
+++ Platformer-master/game_setting.py
@@ -0,0 +1,113 @@
+#Platform, Window, Exit, Coin
+
+import pygame
+from pygame.locals import *
+from game_value import *
+import pickle
+from os import path
+from game_image_sound import *
+from game_role import *
+
+screen = pygame.display.set_mode((screen_width, screen_height), pygame.RESIZABLE)
+
+
+# 각 스프라이트 마다 group을 지정
+blob_group = pygame.sprite.Group()
+platform_group = pygame.sprite.Group()
+lava_group = pygame.sprite.Group()
+coin_group = pygame.sprite.Group()
+exit_group = pygame.sprite.Group()
+
+class World():
+	def __init__(self, data):
+		self.tile_list = []
+
+		#load images
+		dirt_img = pygame.image.load('img/dirt.png')
+		grass_img = pygame.image.load('img/grass.png')
+
+		row_count = 0
+		for row in data:
+			col_count = 0
+			for tile in row:
+				if tile == 1:
+					img = pygame.transform.scale(dirt_img, (tile_size, tile_size))
+					img_rect = img.get_rect()
+					img_rect.x = col_count * tile_size
+					img_rect.y = row_count * tile_size
+					tile = (img, img_rect)
+					self.tile_list.append(tile)
+				if tile == 2:
+					img = pygame.transform.scale(grass_img, (tile_size, tile_size))
+					img_rect = img.get_rect()
+					img_rect.x = col_count * tile_size
+					img_rect.y = row_count * tile_size
+					tile = (img, img_rect)
+					self.tile_list.append(tile)
+				if tile == 3:
+					blob = Enemy(col_count * tile_size, row_count * tile_size + 15)
+					blob_group.add(blob)
+				if tile == 4:
+					platform = Platform(col_count * tile_size, row_count * tile_size, 1, 0)
+					platform_group.add(platform)
+				if tile == 5:
+					platform = Platform(col_count * tile_size, row_count * tile_size, 0, 1)
+					platform_group.add(platform)
+				if tile == 6:
+					lava = Lava(col_count * tile_size, row_count * tile_size + (tile_size // 2))
+					lava_group.add(lava)
+				if tile == 7:
+					coin = Coin(col_count * tile_size + (tile_size // 2), row_count * tile_size + (tile_size // 2))
+					coin_group.add(coin)
+				if tile == 8:
+					exit = Exit(col_count * tile_size, row_count * tile_size - (tile_size // 2))
+					exit_group.add(exit)
+				col_count += 1
+			row_count += 1
+
+	def draw(self):
+		for tile in self.tile_list:
+			screen.blit(tile[0], tile[1]) 
+
+
+class Platform(pygame.sprite.Sprite):
+	def __init__(self, x, y, move_x, move_y):
+		pygame.sprite.Sprite.__init__(self)
+		img = pygame.image.load('img/platform.png')
+		self.image = pygame.transform.scale(img, (tile_size, tile_size // 2))
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+		self.move_counter = 0
+		self.move_direction = 1
+		self.move_x = move_x
+		self.move_y = move_y
+
+	def update(self):
+		self.rect.x += self.move_direction * self.move_x
+		self.rect.y += self.move_direction * self.move_y
+		self.move_counter += 1
+		if abs(self.move_counter) > 50:
+			self.move_direction *= -1
+			self.move_counter *= -1  
+
+
+class Exit(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		img = pygame.image.load('img/exit.png')
+		self.image = pygame.transform.scale(img, (tile_size, int(tile_size * 1.5)))
+		self.rect = self.image.get_rect()
+		self.rect.x = x
+		self.rect.y = y
+
+
+class Coin(pygame.sprite.Sprite):
+	def __init__(self, x, y):
+		pygame.sprite.Sprite.__init__(self)
+		img = pygame.image.load('img/coin.png')
+		self.image = pygame.transform.scale(img, (tile_size // 2, tile_size // 2))
+		self.rect = self.image.get_rect()
+		self.rect.center = (x, y)
+
+
