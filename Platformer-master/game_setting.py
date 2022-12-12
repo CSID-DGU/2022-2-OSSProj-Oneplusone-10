fc64785b25bc9026c7b3a558ccee97215e4f5c2e
hooooooni <kheee2010@dgu.ac.kr>
2022-12-12 13:44:34 +0900

    [22.12.12] Update: 코인->과제물 변경 이미지 추가, 적용

--- Platformer-master/game_setting.py
+++ Platformer-master/game_setting.py
@@ -111,7 +111,7 @@ class Exit(pygame.sprite.Sprite):
 class Coin(pygame.sprite.Sprite):
 	def __init__(self, x, y):
 		pygame.sprite.Sprite.__init__(self)
-		img = pygame.image.load('img/coin.png')
+		img = pygame.image.load('img/homework.png')
 		self.image = pygame.transform.scale(img, (tile_size // 2, tile_size // 2))
 		self.rect = self.image.get_rect()
 		self.rect.center = (x, y)
